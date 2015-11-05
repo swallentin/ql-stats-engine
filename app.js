c18bd0d17b3c12e34eae96c7c9eb08343fff7ed0
Stephan Wallentin <stephan.wallentin@gmail.com>
2015-11-05 07:49:57 +0100

    Improve stability of elo update for duels

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ MongoClient.connect(url, function (err, db) {
                                 winningPlayer,
                                 losingPlayer,
                                 winningPlayerOldElo,
-                                loosingPlayerOldElo,
+                                losingPlayerOldElo,
                                 winningPlayerNewElo,
                                 losingPlayerNewElo;
 
@@ -74,50 +74,55 @@ MongoClient.connect(url, function (err, db) {
                                 },
                                 "DATA.MATCH_GUID": document.DATA.MATCH_GUID
                             }).toArray(function (err, results) {
-                                winningPlayerStats = results[0].DATA.RANK === 1 ? results[0] : results[1];
-                                losingPlayerStats = results[0].DATA.RANK === 2 ? results[0] : results[1];
+                                if (results.length > 1 && results[0].DATA && results[1].DATA )  {
+                                    winningPlayerStats = results[0].DATA.RANK === 1 ? results[0] : results[1];
+                                    losingPlayerStats = results[0].DATA.RANK === 2 ? results[0] : results[1];
 
-                                players.find({
-                                    "DATA.STEAM_ID": {
-                                        $in: [
-                                            winningPlayerStats.DATA.STEAM_ID,
-                                            losingPlayerStats.DATA.STEAM_ID
-                                        ]
-                                    }
-                                }).toArray(function(err, results) {
-                                    winningPlayer = results[0].DATA.STEAM_ID === winningPlayerStats.DATA.STEAM_ID ? results[0] : results[1];
-                                    losingPlayer = results[0].DATA.STEAM_ID === losingPlayerStats.DATA.STEAM_ID ? results[0] : results[1];
+                                    players.find({
+                                        "DATA.STEAM_ID": {
+                                            $in: [
+                                                winningPlayerStats.DATA.STEAM_ID,
+                                                losingPlayerStats.DATA.STEAM_ID
+                                            ]
+                                        }
+                                    }).toArray(function(err, results) {
+                                        winningPlayer = results[0].DATA.STEAM_ID === winningPlayerStats.DATA.STEAM_ID ? results[0] : results[1];
+                                        losingPlayer = results[0].DATA.STEAM_ID === losingPlayerStats.DATA.STEAM_ID ? results[0] : results[1];
 
 
-                                    winningPlayerOldElo = (winningPlayer.ELO && winningPlayer.ELO.DUEL) ?  winningPlayer.ELO.DUEL : 1200;
-                                    loosingPlayerOldElo = (losingPlayer.ELO && losingPlayer.ELO.DUEL) ?  losingPlayer.ELO.DUEL : 1200;
+                                        winningPlayerOldElo = (winningPlayer.ELO && winningPlayer.ELO.DUEL) ?  winningPlayer.ELO.DUEL : 1200;
+                                        losingPlayerOldElo = (losingPlayer.ELO && losingPlayer.ELO.DUEL) ?  losingPlayer.ELO.DUEL : 1200;
 
-                                    winningPlayerNewElo = elo.newRatingIfWon(winningPlayerOldElo, loosingPlayerOldElo);
-                                    losingPlayerNewElo = elo.newRatingIfLost(loosingPlayerOldElo, winningPlayerOldElo);
+                                        winningPlayerNewElo = elo.newRatingIfWon(winningPlayerOldElo, losingPlayerOldElo);
+                                        losingPlayerNewElo = elo.newRatingIfLost(losingPlayerOldElo, winningPlayerOldElo);
 
-                                    var winningPlayerQuery = {
-                                            "DATA.STEAM_ID": winningPlayerStats.DATA.STEAM_ID
-                                        },
-                                        losingPlayerQuery = {
-                                            "DATA.STEAM_ID": losingPlayerStats.DATA.STEAM_ID
-                                        },
-                                        winningPlayerUpdate = {
-                                            $set: {
-                                                ELO: {
-                                                    DUEL: winningPlayerNewElo
+                                        var winningPlayerQuery = {
+                                                "DATA.STEAM_ID": winningPlayerStats.DATA.STEAM_ID
+                                            },
+                                            losingPlayerQuery = {
+                                                "DATA.STEAM_ID": losingPlayerStats.DATA.STEAM_ID
+                                            },
+                                            winningPlayerUpdate = {
+                                                $set: {
+                                                    ELO: {
+                                                        DUEL: winningPlayerNewElo
+                                                    }
                                                 }
-                                            }
-                                        },
-                                        losingPlayerUpdate = {
-                                            $set: {
-                                                ELO: {
-                                                    DUEL: losingPlayerNewElo
+                                            },
+                                            losingPlayerUpdate = {
+                                                $set: {
+                                                    ELO: {
+                                                        DUEL: losingPlayerNewElo
+                                                    }
                                                 }
-                                            }
-                                        };
-                                    players.update(winningPlayerQuery, winningPlayerUpdate);
-                                    players.update(losingPlayerQuery, losingPlayerUpdate);
-                                });
+                                            };
+                                        players.update(winningPlayerQuery, winningPlayerUpdate);
+                                        players.update(losingPlayerQuery, losingPlayerUpdate);
+                                        console.log("win:", winningPlayerStats.DATA.STEAM_ID, 'old elo:', winningPlayerOldElo, 'new elo:', winningPlayerNewElo);
+                                        console.log("loss:", losingPlayerStats.DATA.STEAM_ID, 'old elo:', losingPlayerOldElo, 'new elo:', losingPlayerNewElo);
+                                    });
+                                }
+
                             });
                         }
 
